20679aca751ae421e88e880186ae8e83a5fe453e
fearlesfe <505380967@q.com>
2019-12-29 22:46:30 +0800

    change cert file

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const http_server = http.createServer(app);
 http_server.listen(80, '0.0.0.0');
 
 const options = {
-  key:  fs.readFileSync('./cert/2890117_qiuqiu.site.key'),
-  cert:  fs.readFileSync('./cert/2890117_qiuqiu.site.pem')
+  key:  fs.readFileSync('./cert/2890117_qiuqiu.site0.key'),
+  cert:  fs.readFileSync('./cert/2890117_qiuqiu.site0.pem')
 }
 const https_server = https.createServer(options, app);
 https_server.listen(443, '0.0.0.0');
