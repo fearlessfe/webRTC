f2db8620eb52edd882515fc25d1b14cedb915c37
fearlesfe <505380967@qq.com>
2020-01-31 12:22:52 +0800

    本机1对1视频连接

---
+++ public/peerconnection/js/client.js
@@ -0,0 +1,105 @@
+var localvideo = document.querySelector("video#localvideo")
+var remotevideo = document.querySelector("video#remotevideo")
+
+var btnStart = document.querySelector("button#start")
+var btnCall = document.querySelector("button#call")
+var btnHangup = document.querySelector("button#hangup")
+
+var localStream;
+var pc1;
+var pc2;
+
+function getMediaStream(stream) {
+  localvideo.srcObject = stream;
+  localStream = stream;
+}
+
+function handleError(err) {
+  console.error('Failed to get Media Stream')
+}
+
+function handleOfferError(err) {
+  console.error('Failed to get Local Offer')
+}
+
+function handleAnswerError(err) {
+  console.error('Failed to get Local Answer')
+}
+
+function start() {
+  if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    console.error('the getUserMedia is not supported')
+    return;
+  }
+  var constraints = {
+    video: true,
+    audio: false
+  }
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(getMediaStream)
+    .catch(handleError)
+}
+
+function getRemoteStream(e) {
+  remotevideo.srcObject = e.streams[0];
+}
+
+function getOffer(desc) {
+  pc1.setLocalDescription(desc);
+
+  // send desc to signal
+  // pc2 receive desc from singal
+
+  pc2.setRemoteDescription(desc);
+
+  pc2.createAnswer()
+    .then(getAnswer)
+    .catch(handleAnswerError);
+}
+
+function getAnswer(desc) {
+  pc2.setLocalDescription(desc);
+
+  // send desc to signal
+  // pc1 receive desc from singal
+  pc1.setRemoteDescription(desc);
+}
+
+function call() {
+  pc1 = new RTCPeerConnection();
+  pc2 = new RTCPeerConnection();
+
+  pc1.onicecandidate = (e) => {
+    pc2.addIceCandidate(e.candidate)
+  }
+
+  pc2.onicecandidate = (e) => {
+    pc1.addIceCandidate(e.candidate)
+  }
+
+  pc2.ontrack = getRemoteStream;
+
+  localStream.getTracks().forEach(track => {
+    pc1.addTrack(track, localStream)
+  })
+
+  var offerOptions = {
+    offerToRecieveAudio: 0,
+    offerToRecieveVideo: 1,
+  }
+  pc1.createOffer(offerOptions)
+    .then(getOffer)
+    .catch(handleOfferError);
+
+}
+
+function hangup() {
+  pc1.close();
+  pc2.close();
+  pc1 = null;
+  pc2 = null;
+}
+
+btnStart.onclick = start;
+btnCall.onclick = call;
+btnHangup.onclick = hangup;
